4a9b1922262b8eec63f44a01c8259ebfdca096f3
Russell Balahadia <russel.balahadia@gmail.com>
2023-05-09 17:44:00 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,12 @@
 variable "region" {
     default = "ap-southeast-1"
 }
+variable "access_key"{
+    default = "{secrets.access_key}"
+}
+variable "secret_key"{
+    default = "{secrets.secret_key}"
+}
 variable "VPC_CIDR_Block"{
     default = "10.0.0.0/16"
 }
