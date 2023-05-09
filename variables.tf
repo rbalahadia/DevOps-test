386c70989b1011c952f62b67994d26423e84b95f
Russell Balahadia <russel.balahadia@gmail.com>
2023-05-09 18:32:21 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "public_subnet_cidr"{
     default = "10.0.0.0/24"
 }
 variable "public_subnet_cidr2"{
-    default = "10.0.2.0/24"
+    default = "10.0.2.0/26"
 }
 variable "private_subnet_cidr"{
     default = "10.0.1.0/24"
