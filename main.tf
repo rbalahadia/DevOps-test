fa7d1f4c203d9e2396a7906b843e8599766ce0bb
unknown <rbalahadia@crescendocollective.com>
2023-05-09 19:14:11 +0800

    test

--- main.tf
+++ main.tf
@@ -23,6 +23,37 @@ resource "aws_vpc" "Terraform_VPC"{
 
     }
 
+resource "aws_iam_role" "Tabist_EC2_Role" {
+  name = "Tabist_EC2_Role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Principal": {
+                "Service": [
+                    "ec2.amazonaws.com"
+                ]
+            }
+        }
+    ]
+})
+
+  tags = {
+    tag-key = "Tabist EC2 role"
+  }
+}
+
+resource "aws_iam_instance_profile" "Tabist_instance_profile" {
+  name = "Tabist_instance_profile"
+  role = "${aws_iam_role.Tabist_EC2_Role.name}"
+}
 
 resource "aws_subnet" "Terraform_Public_Subnet_1a"{
     vpc_id = aws_vpc.Terraform_VPC.id
@@ -155,9 +186,21 @@ resource "aws_instance" "tabist_EC2_1" {
   associate_public_ip_address = true
   subnet_id = aws_subnet.Terraform_Public_Subnet_1a.id
   vpc_security_group_ids = [aws_security_group.Tabist_Security_group.id]
+  user_data = <<EOF
+  #!/bin/bash
+  echo "installing SSM agent"
+  sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+  service amazon-ssm-agent start
+  yum install docker -y
+  sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  service docker start
+  EOF
+  iam_instance_profile = "${aws_iam_instance_profile.Tabist_instance_profile.name}"
   tags = {
     Name = "Tabist_EC2"
   }
+  
 }
 resource "aws_instance" "tabist_EC2_2" {
   ami           = "ami-052f483c20fa1351a"
