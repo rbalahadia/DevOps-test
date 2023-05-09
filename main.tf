e07eb87993b85c3afb525cc1f437d695fc321111
Russell Balahadia <russel.balahadia@gmail.com>
2023-05-09 20:45:15 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -163,7 +163,7 @@ resource "aws_security_group" "Tabist_Security_group" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [var.public_subnet_cidr]
+    cidr_blocks      = [var.public_subnet_cidr,"49.144.200.29/32"]
   }
 
   egress {
