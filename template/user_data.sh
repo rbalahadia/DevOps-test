fc885c70c436eeccd5655699a0efd3fd8ba76a55
unknown <rbalahadia@crescendocollective.com>
2023-05-10 22:18:43 +0800

    new version

--- template/user_data.sh
+++ template/user_data.sh
@@ -17,11 +17,12 @@ cd dockerfiles
 echo "FROM openjdk:20-slim-buster
 WORKDIR /app
 COPY . .
-CMD ./bin/zookeeper-server-start.sh config/zookeeper.properties" > Dockerfile.zookeeper
+CMD kafka_2.13-3.4.0/bin/zookeeper-server-start.sh config/zookeeper.properties" > Dockerfile.zookeeper
+
 echo "FROM openjdk:20-slim-buster
 WORKDIR /app
 COPY . .
-CMD ./bin/zookeeper-server-start.sh config/zookeeper.properties" > Dockerfile.kafka
+CMD kafka_2.13-3.4.0/bin/kafka-server-start.sh config/server.properties" > Dockerfile.kafka
 cd ..
 echo "# the directory where the snapshot is stored.
 dataDir=/tmp/zookeeper
@@ -33,69 +34,38 @@ maxClientCnxns=0
 # Set the port to something non-conflicting if choosing to enable this
 admin.enableServer=false
 # admin.serverPort=8080" > configfiles/zookeeper.properties
-echo "version: "3.9"
-
-networks:
-  default:
-    name: kafka
-    driver: bridge
-
-volumes:
-  zookeeperdata:
+echo "version: \"3.3\"
 
 services:
   zookeeper:
-    build:
-      context: kafka_2.13-3.4.0
-      dockerfile: ../dockerfiles/Dockerfile.zookeeper
+    image: zookeeper:latest
     volumes:
-      - zookeeperdata:/tmp/zookeeper
-      - ./configfiles/zookeeper.properties:/app/config/zookeeper.properties
-  kafka:
-    build: 
-      context: kafka_2.13-3.4.0
-      dockerfile: ../dockerfiles/Dockerfile.kafka
-    volumes:
-      - kafka-server-data:/tmp/kafka-logs
-      - ./configfiles/kafka-server/server.properties:/app/config/server.properties
+      - /opt/kafka-cluster/zookeeperdata/:/tmp/zookeeper/
+      - /opt/kafka-cluster/configfiles/zookeeper.properties:/app/config/zookeeper.properties
     ports:
-      - 4000:4000" > docker-compose.yml
-docker-compose up -d --build zookeeper
+      - 14000:14000
+  kafka:
+    image: kafka-server:latest
+    depends_on:
+      - zookeeper
+    volumes:
+      - /opt/kafka-cluster/kafka-server-data/:/tmp/kafka-logs/
+      - /opt/kafka-cluster/configfiles/kafka-server/server.properties:/app/config/server.properties
+    ports:
+      - 9092:9092" > docker-compose.yml
+docker-compose up -d
 mkdir configfiles/kafka-server
 echo "broker.id=0
-zookeeper.connect=zookeeper:14000
-zookeeper.connection.timeout.ms=18000
-sasl.enabled.mechanisms=PLAIN
-sasl.mechanism.inter.broker.protocol=PLAIN
-listener.name.internal.plain.sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required \
- username="admin" \
- password="admin-secret" \
- user_admin="admin-secret";
-
-listener.name.external.plain.sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required \
-     username="admin" \
-     password="admin-secret" \
- user_admin="admin-secret";
-
-listeners=INTERNAL://:9092,EXTERNAL://:4000
-advertised.listeners=INTERNAL://leesin:9092,EXTERNAL://10.0.2.15:4000
-inter.broker.listener.name=INTERNAL
-listener.security.protocol.map=INTERNAL:SASL_PLAINTEXT,EXTERNAL:SASL_PLAINTEXT
 num.network.threads=3
 num.io.threads=8
-socket.send.buffer.bytes=102400
-socket.receive.buffer.bytes=102400
-socket.request.max.bytes=104857600
 log.dirs=/tmp/kafka-logs
-num.partitions=3
+num.partitions=1
 num.recovery.threads.per.data.dir=1
 offsets.topic.replication.factor=1
 transaction.state.log.replication.factor=1
 transaction.state.log.min.isr=1
-log.flush.interval.messages=10000
-log.flush.interval.ms=1000
-log.retention.hours=168
-log.segment.bytes=1073741824
 log.retention.check.interval.ms=300000
-group.initial.rebalance.delay.ms=0" > configfiles/kafka-server/server.properties
+zookeeper.connect=localhost:14000
+zookeeper.connection.timeout.ms=18000" > configfiles/kafka-server/server.properties
 docker compose up -d kafka
+yum install java-11* -y
