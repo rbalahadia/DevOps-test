4d3eab7ce671ec811e80af1dded0f348605b6d1a
unknown <rbalahadia@crescendocollective.com>
2023-05-11 16:20:39 +0800

    final terraform

--- template/user_data.sh
+++ template/user_data.sh
@@ -21,15 +21,16 @@ CMD kafka_2.13-3.4.0/bin/zookeeper-server-start.sh config/zookeeper.properties" 
 
 echo "FROM openjdk:20-slim-buster
 WORKDIR /app
-COPY . .
-CMD kafka_2.13-3.4.0/bin/kafka-server-start.sh config/server.properties" > Dockerfile.kafka
-docker build -t zookeeper:latest -f Dockerfile.zookeeper .
-docker build -t kafka-server:latest -f Dockerfile.kafka .
+COPY kafka_2.13-3.4.0/ .
+COPY configfiles/kafka-server/ config/
+CMD bin/kafka-server-start.sh config/server.properties" > Dockerfile.kafka
 cd ..
+docker build -t zookeeper:latest -f dockerfiles/Dockerfile.zookeeper .
+docker build -t kafka-server:latest -f dockerfiles/Dockerfile.kafka .
 echo "# the directory where the snapshot is stored.
 dataDir=/tmp/zookeeper
 # the port at which the clients will connect
-clientPort=14000
+clientPort=2181
 # disable the per-ip limit on the number of connections since this is a non-production config
 maxClientCnxns=0
 # Disable the adminserver by default to avoid port conflicts.
@@ -37,6 +38,8 @@ maxClientCnxns=0
 admin.enableServer=false
 # admin.serverPort=8080" > configfiles/zookeeper.properties
 echo "version: \"3.3\"
+networks:
+  default:
 
 services:
   zookeeper:
@@ -45,16 +48,16 @@ services:
       - /opt/kafka-cluster/zookeeperdata/:/tmp/zookeeper/
       - /opt/kafka-cluster/configfiles/zookeeper.properties:/app/config/zookeeper.properties
     ports:
-      - 14000:14000
+      - 2181:2181
   kafka:
     image: kafka-server:latest
     depends_on:
       - zookeeper
     volumes:
       - /opt/kafka-cluster/kafka-server-data/:/tmp/kafka-logs/
-      - /opt/kafka-cluster/configfiles/kafka-server/server.properties:/app/config/server.properties
+#      - /opt/kafka-cluster/configfiles/kafka-server/server.properties:/app/config/server.properties
     ports:
-      - 4000:4000" > docker-compose.yml
+      - 9092:9092" > docker-compose.yml
 docker-compose up -d
 mkdir configfiles/kafka-server
 echo "broker.id=0
@@ -67,9 +70,9 @@ offsets.topic.replication.factor=1
 transaction.state.log.replication.factor=1
 transaction.state.log.min.isr=1
 log.retention.check.interval.ms=300000
-zookeeper.connect=localhost:14000
+zookeeper.connect=54.251.1.97:2181
 zookeeper.connection.timeout.ms=18000
-listeners=INTERNAL://:9092,EXTERNAL://:4000
-advertised.listeners=INTERNAL://kafka:9092,EXTERNAL://localhost:4000" > configfiles/kafka-server/server.properties
+listeners=PLAINTEXT://0.0.0.0:9092
+advertised.listeners=PLAINTEXT://54.251.1.97:9092" > configfiles/kafka-server/server.properties
 docker compose up -d kafka
 yum install java-11* -y
